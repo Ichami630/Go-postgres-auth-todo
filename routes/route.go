0323184df904ce971bb89f718f8c9fa60ca5c03e
ichami630 <brandonichami@gmail.com>
2025-03-29 04:42:39 +0100

    frontpage,login,signup ready

--- routes/route.go
+++ routes/route.go
@@ -5,8 +5,18 @@ import "github.com/gin-gonic/gin"
 func Router() {
 	server := gin.Default() //create a gin router with default middleware(logger and recovery)
 
+	server.Static("./assets", "./views/assets") //serve static files like css,js,images
+
+	server.LoadHTMLGlob("views/*.html") //load html files from the views folder
+
 	server.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "welcome"})
+		c.HTML(200, "index.html", nil)
+	})
+	server.GET("/login", func(c *gin.Context) {
+		c.HTML(200, "login.html", nil)
+	})
+	server.GET("/signup", func(c *gin.Context) {
+		c.HTML(200, "signup.html", nil)
 	})
 
 	server.Run(":8080") //listening on port 8080
